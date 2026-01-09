d8c4522cefb7a5e7870ba30dd3f24c8a7a7f1c96
partik <pnain359@gmail.com>
2026-01-09 22:10:22 +0530

    added problem of maximum sum of rows in  a matrix

---
+++ 2Darray/maxrowsum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int rowssum(int matrix[][3],int rows,int cols){
+    int maxsum= INT8_MIN;
+    for(int i=0;i<rows;i++){
+        int sum=0;
+        for(int j=0;j<cols;j++){
+            sum+=matrix[i][j];
+        }
+        maxsum=max(maxsum,sum);
+    }
+    return maxsum;
+}
+int main (){
+    int matrix[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int rows=3;
+    int cols=3;
+    cout<<"maximum sum of rows in matrix is"<<rowssum(matrix,rows,cols)<<endl;
+}
