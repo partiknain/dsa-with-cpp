2dd0479a7e96b5b2cb15f4586307bd04b38363d8
partik <pnain359@gmail.com>
2026-01-10 21:34:32 +0530

    added problem of search in 2d matrix leetcode problem

---
+++ 2Darray/search.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool searchRow(vector<vector<int>>& matrix, int row, int target) {
+    int n = matrix[0].size();
+    int st = 0, end = n - 1;
+
+    while (st <= end) {
+        int mid = st + (end - st) / 2;
+        if (matrix[row][mid] == target)
+            return true;
+        else if (matrix[row][mid] < target)
+            st = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return false;
+}
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    int srow = 0, erow = m - 1;
+
+    while (srow <= erow) {
+        int mid = srow + (erow - srow) / 2;
+
+        if (matrix[mid][0] <= target && matrix[mid][n - 1] >= target) {
+            return searchRow(matrix, mid, target);
+        }
+        else if (target > matrix[mid][n - 1]) {
+            srow = mid + 1;
+        }
+        else {
+            erow = mid - 1;
+        }
+    }
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    int target = 3;
+
+    if (searchMatrix(matrix, target))
+        cout << "Found";
+    else
+        cout << "Not Found";
+}
