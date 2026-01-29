934d4db478276ef15eedfe26f03235d4a38f672e
partik <pnain359@gmail.com>
2026-01-29 23:31:45 +0530

    updated 3_sum.cpp

--- hashing/3_sum.cpp
+++ hashing/3_sum.cpp
@@ -2,42 +2,59 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-vector<vector<int>>triplet(vector<int>nums){
-sort(nums.begin(),nums.end());
-int n=nums.size();
-vector<vector<int>>ans;
-for(int i=0;i<n;i++){
-    if(i>0 && nums[i]==nums[i-1]) continue;
-    int j=i+1,k=n-1;
-    while(j<k){
-        int sum=nums[i]+nums[j]+nums[k];
-        if(sum<0){
-            j++;
-        }
-        else if(sum>0){
-            k--;
-        }
-        else{
-            ans.push_back({nums[i],nums[j],nums[k]});
-            j++;
-            k--;
-            if(j<k  && nums[j]==nums[j+1])  j++;
-        if(j<k  && nums[k]==nums[k-1])  k--;
+
+vector<vector<int> > triplet(vector<int> nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    vector<vector<int> > ans;
+
+    for(int i = 0; i < n; i++) {
+        if(i > 0 && nums[i] == nums[i-1]) continue;
+
+        int j = i + 1, k = n - 1;
+
+        while(j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+
+            if(sum < 0) j++;
+            else if(sum > 0) k--;
+            else {
+               vector<int> temp;
+            temp.push_back(nums[i]);
+            temp.push_back(nums[j]);
+            temp.push_back(nums[k]);
+            ans.push_back(temp);
+
+                j++;
+                k--;
+
+                while(j < k && nums[j] == nums[j-1]) j++;
+                while(j < k && nums[k] == nums[k+1]) k--;
             }
         }
-    
     }
     return ans;
 }
-int main (){
-    vector<int>nums={-1,1,2,3,4,-2};
-    vector<vector<int>>ans=triplet(nums);
-    for(auto &v:ans){
-        cout<<"[";
-        for(int x:v)  cout<<x<<" ";
-        cout<<"]";
+
+int main() {
+    vector<int> nums;
+     nums.push_back(-1);
+     nums.push_back(1);
+     nums.push_back(2);
+     nums.push_back(3);
+     nums.push_back(4);
+     nums.push_back(-2);
+
+    vector<vector<int> > ans = triplet(nums);
+
+    for(int i = 0; i < ans.size(); i++) {
+    cout << "[ ";
+    for(int j = 0; j < ans[i].size(); j++) {
+        cout << ans[i][j] << " ";
     }
-return 0;
+    cout << "]";
 }
-//time complexity = O(n^2 * log(unique trilets))
-//space complexity = O(unique triplets +n)
+return 0;
+    }
+    
+
