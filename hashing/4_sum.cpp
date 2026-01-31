5e43b8c9b54f5c40afdd74e56a5e08825f286bb1
partik <pnain359@gmail.com>
2026-01-31 23:08:20 +0530

    updated 4_sum.cpp

--- hashing/4_sum.cpp
+++ hashing/4_sum.cpp
@@ -1,58 +1,55 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int> > triplet(vector<int> nums) {
-    sort(nums.begin(), nums.end());
+int main() {
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(0);
+    nums.push_back(-1);
+    nums.push_back(0);
+    nums.push_back(-2);
+    nums.push_back(2);
+
+    int target = 0;
     int n = nums.size();
-    vector<vector<int> >ans;
-    for(int i = 0; i < n; i++) {
-        if(i > 0 && nums[i] == nums[i-1]) continue;
 
-        int j = i + 1, k = n - 1;
+    sort(nums.begin(), nums.end());
 
-        while(j < k) {
-            int sum = nums[i] + nums[j] + nums[k];
+    for (int i = 0; i < n - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            if(sum < 0) j++;
-            else if(sum > 0) k--;
-            else {
-               vector<int> temp;
-            temp.push_back(nums[i]);
-            temp.push_back(nums[j]);
-            temp.push_back(nums[k]);
-            ans.push_back(temp);
+        for (int j = i + 1; j < n - 2; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
-                j++;
-                k--;
+            int left = j + 1;
+            int right = n - 1;
 
-                while(j < k && nums[j] == nums[j-1]) j++;
-                while(j < k && nums[k] == nums[k+1]) k--;
+            while (left < right) {
+                int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum == target) {
+                    cout << nums[i] << " "
+                         << nums[j] << " "
+                         << nums[left] << " "
+                         << nums[right] << endl;
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if (sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
             }
         }
     }
-    return ans;
+
+    return 0;
 }
-int main() {
-    vector<int> nums;
-     nums.push_back(-1);
-     nums.push_back(1);
-     nums.push_back(2);
-     nums.push_back(3);
-     nums.push_back(4);
-     nums.push_back(-2);
-
-    vector<vector<int> > ans = triplet(nums);
-
-    for(int i = 0; i < ans.size(); i++) {
-    cout << "[ ";
-    for(int j = 0; j < ans[i].size(); j++) {
-        cout << ans[i][j] << " ";
-    }
-    cout << "]";
-}
-return 0;
-    }
-    
-
