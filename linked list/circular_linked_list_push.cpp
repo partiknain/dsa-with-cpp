82959ee37f84dadba00ba673fd203367ed03455c
partik <pnain359@gmail.com>
2026-03-11 19:04:57 +0530

    added all push operation of a circular linked list

---
+++ linked list/circular_linked_list_push.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class node{
+public:
+int data;
+node* next;
+node(int val){
+data=val;
+next=NULL;
+}
+};
+class circularlist{
+node* head;
+node* tail;
+public:
+circularlist(){
+    head=tail=NULL;
+}
+void push_front(int val){
+    node* newnode=new node(val);
+    if(head==NULL){
+        head=tail=newnode;
+        tail->next=head;
+    }
+    else{
+        newnode->next=head;
+        head=newnode;
+        tail->next=head;
+    }
+}
+void print(){
+    if(head==NULL) return ;
+    cout<<head->data<<"->";
+    node* temp=head->next;
+    while(temp!=head){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<temp->data<<"->NULL";
+}
+};
+int main (){
+    circularlist l;
+    l.push_front(3);
+    l.push_front(2);
+    l.push_front(1);
+    l.print();
+    return 0;
+}
